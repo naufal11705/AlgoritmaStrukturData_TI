8f53598b1d25700a0053cc00d97ba04db296439c
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-03-13 10:23:30 +0700

    delete kuis

--- Pertemuan 4 - Kuis/Bola.java
+++
@@ -1,8 +0,0 @@
-import java.lang.Math;
-
-public class Bola {
-
-    public double Volume(double r) {
-        return 4/3 * Math.PI * r * r * r;
-    }
-}
--- Pertemuan 4 - Kuis/BolaMain.java
+++
@@ -1,9 +0,0 @@
-public class BolaMain {
-    public static void main(String[] args) {
-        Bola[] arrBola = new Bola[2];
-
-        for(int i=0; i<arrBola.length; i++) {
-            arrBola[i] = new Bola();
-        }
-    }
-}
