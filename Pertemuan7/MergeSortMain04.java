e29da79618d8003568dd1b29b81337f84c90ca4b
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-03-28 17:09:42 +0700

    add: MergeSortMain04.java

---
+++ Pertemuan7/MergeSortMain04.java
@@ -0,0 +1,14 @@
+package Pertemuan7;
+
+public class MergeSortMain04 {
+    public static void main(String[] args) {
+        int data[] = {10,40,30,50,70,20,100,90};
+        System.out.println("Sorting dengan merge sort");
+        MergeSorting04 mSort = new MergeSorting04();
+        System.out.println("Data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("Setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
