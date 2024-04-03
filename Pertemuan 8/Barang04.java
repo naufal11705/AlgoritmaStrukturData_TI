0ff95253242903acf30c31ca8c9f68708863ac65
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-04-03 11:24:09 +0700

    add: Barang04.java

---
+++ Pertemuan 8/Barang04.java
@@ -0,0 +1,11 @@
+public class Barang04 {
+    int kode;
+    String nama, kategori;
+
+    Barang04(int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+    
+}
