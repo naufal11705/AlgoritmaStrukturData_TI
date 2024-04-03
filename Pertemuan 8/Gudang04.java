15cf112cdcbc6eced1d816f24fa1eb4cc832e0fe
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-04-03 11:24:17 +0700

    add: Gudang04.java

---
+++ Pertemuan 8/Gudang04.java
@@ -0,0 +1,72 @@
+public class Gudang04 {
+    Barang04[] tumpukan;
+    int size;
+    int top;
+
+    public Gudang04(int kapasitas) {
+        size = kapasitas;
+        tumpukan = new Barang04[size];
+        top = -1;
+    }
+
+    public boolean cekKosong() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cekPenuh() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void tambahBarang(Barang04 brg) {
+        if (!cekPenuh()) {
+            top++;
+            tumpukan[top] = brg;
+            System.out.println("Barang " + brg.nama + " berhasil ditambahkan ke Gudang");
+        } else {
+            System.out.println("Gagal! Tumpukan barang di Gudang sudah penuh");
+        }
+    }
+
+    public Barang04 ambilBarang() {
+        if (!cekKosong()) {
+            Barang04 delete = tumpukan[top];
+            top--;
+            System.out.println("Barang " + delete.nama + " diambil dari Gudang.");
+            return delete;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public Barang04 lihatBarangTeratas() {
+        if (!cekKosong()) {
+            Barang04 barangTeratas = tumpukan[top];
+            System.out.println("Barang teratas: " + barangTeratas.nama);
+            return barangTeratas;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public void tampilkanBarang() {
+        if (!cekKosong()) {
+            System.out.println("Rincian tumpukan barang di Gudang:");
+            for (int i=0; i<=top; i++) {
+                System.out.printf("Kode %d: %s (Kategori %s)\n", tumpukan[i].kode,
+                        tumpukan[i].nama, tumpukan[i].kategori);
+            }
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+        }
+    }
+}
