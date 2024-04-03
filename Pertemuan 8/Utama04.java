afba406bd4449f483d4791b52cf04180a84309ab
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-04-03 11:24:34 +0700

    add: Utama04.java

---
+++ Pertemuan 8/Utama04.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Utama04 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan Kapasitas Gudang: ");
+        Gudang04 gudang = new Gudang04(scanner.nextInt());
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Tambah barang");
+            System.out.println("2. Ambil barang");
+            System.out.println("3. Tampilkan tumpukan barang");
+            System.out.println("4. Lihat barang teratas");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih Operasi: ");
+            int pilihan = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan kode barang: ");
+                    int kode = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Masukkan nama barang: ");
+                    String nama = scanner.nextLine();
+                    System.out.print("Masukkan nama kategori: ");
+                    String kategori = scanner.nextLine();
+                    Barang04 barangBaru = new Barang04(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+
+                case 2:
+                    gudang.ambilBarang();
+                    break;
+
+                case 3:
+                    gudang.tampilkanBarang();
+                    break;
+
+                case 4:
+                    gudang.lihatBarangTeratas();
+                    break;
+
+                case 5:
+                    System.exit(0);
+                    break;
+            
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi");;
+            }
+        }
+    }
+}
