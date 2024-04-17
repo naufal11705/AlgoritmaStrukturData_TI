572dca84f9c4cb8b1320c2070e4677877c5c1d06
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-04-17 11:41:29 +0700

    add: Kasus04.java

---
+++ UTS/Kasus04.java
@@ -0,0 +1,12 @@
+public class Kasus04 {
+    int nilai;
+
+    public Kasus04(int n){
+        this.nilai = n;
+    }
+
+    public void tampilDataKasus() {
+        System.out.print(nilai + " ");
+    }
+
+}
